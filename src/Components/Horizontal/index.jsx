99a2c1cc8835a4deb683c687857a24917ecd1c73
Srikanth Saindla <110750154+SrikanthSaindla@users.noreply.github.com>
2023-07-17 20:36:49 +0530

    six  commit

--- src/Components/Horizontal/index.jsx
+++ src/Components/Horizontal/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import HorizontalScroll from "react-scroll-horizontal";
 import "./index.css"
 
 const  Horizontal = () => {
@@ -6,44 +7,50 @@ const  Horizontal = () => {
     <>
       <div className="container">
         <h1 className="our_journey ">Our Journey</h1>
-        <div className="list_container6">
-          <div className="one">
-            <span className="year">1998</span>
-            <span className="journey">eLogic inception</span>
+        <HorizontalScroll
+          reverseScroll={true}
+          pageLock={true}
+          config={{ stiffness: 170, damping: 26 }} animValues={10}
+        >
+          <div className="list_container6">
+            <div className="one">
+              <span className="year">1998</span>
+              <span className="journey">eLogic inception</span>
+            </div>
+            <div className="two">
+              <span className="year">1999</span>
+              <span className="journey">100% export oriented unit</span>
+            </div>
+            <div className="one">
+              <span className="year">2001</span>
+              <span className="journey">Us Entity Incorporation</span>
+            </div>
+            <div className="two">
+              <span className="year">2002</span>
+              <span className="journey">One Stop Service Provider</span>
+            </div>
+            <div className="one">
+              <span className="year">2005</span>
+              <span className="journey">Global Leaders</span>
+            </div>
+            <div className="two">
+              <span className="year">2015</span>
+              <span className="journey">Incorporated in Dubai</span>
+            </div>
+            <div className="one">
+              <span className="year">2015</span>
+              <span className="journey">AIA registered</span>
+            </div>
+            <div className="two">
+              <span className="year">2019</span>
+              <span className="journey">AISC registered</span>
+            </div>
+            <div className="one">
+              <span className="year">2021</span>
+              <span className="journey">eLogicTech Edge inception</span>
+            </div>
           </div>
-          <div className="two">
-            <span className="year">1999</span>
-            <span className="journey">100% export oriented unit</span>
-          </div>
-          <div className="one">
-            <span className="year">2001</span>
-            <span className="journey">Us Entity Incorporation</span>
-          </div>
-          <div className="two">
-            <span className="year">2002</span>
-            <span className="journey">One Stop Service Provider</span>
-          </div>
-          <div className="one">
-            <span className="year">2005</span>
-            <span className="journey">Global Leaders</span>
-          </div>
-          <div className="two">
-            <span className="year">2015</span>
-            <span className="journey">Incorporated in Dubai</span>
-          </div>
-          <div className="one">
-            <span className="year">2015</span>
-            <span className="journey">AIA registered</span>
-          </div>
-          <div className="two">
-            <span className="year">2019</span>
-            <span className="journey">AISC registered</span>
-          </div>
-          <div className="one">
-            <span className="year">2021</span>
-            <span className="journey">eLogicTech Edge inception</span>
-          </div>
-        </div>
+        </HorizontalScroll>
       </div>
     </>
   );
